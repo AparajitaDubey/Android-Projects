802d606329fa8aba03aacb3f58ef98923c21d136
Aparajita Dubey <apaparajita.dubey88@gmail.com>
2018-09-11 20:49:59 -0700

    Adding Basic Android programs

---
+++ FirstGameApp/app/src/main/java/com/test/firstgameapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.test.firstgameapp
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.util.Log.d
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+
+        floatingButton.setOnClickListener{
+            d("Appy","Pressed!")
+            welcomeMessage.text= "Welcome Android!"
+        }
+
+        backButton.setOnClickListener {
+           welcomeMessage.text="U clicked on Back Button!!!!"
+        }
+
+
+    }
+}
